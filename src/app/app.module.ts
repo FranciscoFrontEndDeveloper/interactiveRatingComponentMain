d786c1fd12b42d9607a993fb30b518b81d4f8d57
FRANCISCO <pachonetworking@gmail.com>
2023-05-15 19:31:43 -0500

    layout created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
+import { CardmainComponent } from './components/cardmain/cardmain.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    CardmainComponent
   ],
   imports: [
     BrowserModule,
